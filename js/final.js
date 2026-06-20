396aa248cd96cf3d65d082bf39d7b17668a8608c
Alexandr Feofanov <113620461+usersasha18@users.noreply.github.com>
2026-06-20 23:46:57 +1000

    Update final.js

--- js/final.js
+++ js/final.js
@@ -1,39 +1,23 @@
 async function showFinalModal() {
-    const element = document.querySelector('#score-content');
+  const jsonString = localStorage.getItem('quizResult');
+  const myObject = JSON.parse(jsonString);
 
-    const jsonString = localStorage.getItem('quizResult');
-    const myObject = JSON.parse(jsonString);
+  await fetch("https://viktorina-backend.onrender.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: myObject.name,
+      score: myObject.score,
+      wrong: myObject.wrong
+    })
+  });
 
-    try {
-        const response = await fetch(
-            "https://viktorina-backend.onrender.com/submit",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                 body: JSON.stringify({
-                  answer: JSON.stringify(myObject)
-                })
-            }
-        );
-
-        console.log(await response.json());
-
-    } catch (err) {
-        console.error("Ошибка отправки:", err);
-    }
-
-    element.innerHTML = `
-        <div class="score-content">
-            <h2>🏁 Квиз завершён</h2>
-
-            <p><b>${myObject.name}</b></p>
-            <p>Всего вопросов ${myObject.score}</p>
-            <p>✔ Правильных ответов: ${myObject.score - myObject.wrong}</p>
-            <p>❌ Неправильных ответов: ${myObject.wrong}</p>
-        </div>
-    `;
+  document.querySelector('#score-content').innerHTML = `
+    <h2>🏁 Квиз завершён</h2>
+    <p><b>${myObject.name}</b></p>
+    <p>Всего вопросов ${myObject.score}</p>
+    <p>❌ Ошибок: ${myObject.wrong}</p>
+  `;
 }
-
-showFinalModal();
